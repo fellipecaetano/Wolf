3b53c3fca795ece6378d87a7660dd66d9a019063
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-07-29 15:20:05 -0300

    Storyboard-management tests won't use Quick anymore

---
+++ Example/Tests/StoryboardsTests.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+import Quick
+import Nimble
+import Wolf
+
+class StoryboardsTests: XCTestCase {
+    func testViewControllerInstantiation() {
+        let viewController: TestViewController = Storyboard.Test.instantiateViewController()
+        expect(String(viewController.dynamicType)).to(equal("TestViewController"))
+    }
+}
+
+class TestViewController: UIViewController, Identifiable {}
+
+private enum Storyboard: String, StoryboardConvertible {
+    case Test
+
+    private var bundle: NSBundle? {
+        return NSBundle(forClass: StoryboardsTests.self)
+    }
+}
