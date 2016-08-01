a1caff62622f54b7d11cedbf1eee084916f22aad
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-08-01 10:52:07 -0300

    Reusable view management and support

--- Wolf/Classes/Reuse/NibLoadable.swift
+++ Wolf/Classes/Reuse/NibLoadable.swift
@@ -10,11 +10,11 @@ extension NibLoadable {
     static var name: String {
         return String(Self)
     }
-    
+
     static var bundle: NSBundle? {
         return nil
     }
-    
+
     static var nib: UINib {
         return UINib(nibName: self.name, bundle: self.bundle)
     }
