5d1d4b276dba2ad28c366f5c45e5dccab960015a
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-08-02 14:23:38 -0300

    Ignoring the "Pods" directory

--- Example/Pods/Target Support Files/Pods-Wolf_Example-Wolf_Tests/Pods-Wolf_Example-Wolf_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Wolf_Example_Wolf_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Wolf_Example_Wolf_Tests
-@end
--- Example/Pods/Target Support Files/Pods-Wolf_Example-Wolf_Tests/Pods-Wolf_Example-Wolf_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Wolf_Example_Wolf_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Wolf_Example_Wolf_TestsVersionString[];
-
