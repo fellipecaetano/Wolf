c55f9f6e6a39b8779dc8af2ea3dd73a54757db67
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-08-04 11:51:57 -0300

    Removed cache code duplication

--- Wolf/Classes/Cache/Networking+Cache.swift
+++ Wolf/Classes/Cache/Networking+Cache.swift
@@ -6,16 +6,8 @@ public extension HTTPClient {
 
         return request(resource)
             .validate()
-            .response(responseSerializer: resource.responseSerializer) { response in
-                if let request = response.request, httpResponse = response.response, data = response.data {
-                    let cachedResponse = CachedResponse(response: httpResponse,
-                                                        data: data,
-                                                        duration: 30,
-                                                        storagePolicy: resource.cacheStoragePolicy)
-                    cachedResponse.store(for: request, cache: resource.cache)
-                }
-                completionHandler(response)
-        }
+            .response(responseSerializer: resource.responseSerializer,
+                      completionHandler: cache(resource, completionHandler))
     }
 
     func sendArrayRequest<R: protocol<HTTPResource, CacheableResource>>
@@ -23,16 +15,23 @@ public extension HTTPClient {
 
         return request(resource)
             .validate()
-            .response(responseSerializer: resource.arrayResponseSerializer) { response in
-                if let request = response.request, httpResponse = response.response, data = response.data {
-                    let cachedResponse = CachedResponse(response: httpResponse,
-                                                        data: data,
-                                                        duration: 30,
-                                                        storagePolicy: resource.cacheStoragePolicy)
-                    cachedResponse.store(for: request, cache: resource.cache)
-                }
-                completionHandler(response)
+            .response(responseSerializer: resource.arrayResponseSerializer,
+                      completionHandler: cache(resource, completionHandler))
+    }
+}
+
+private func cache<R: protocol<HTTPResource, CacheableResource>, V, E: ErrorType>
+    (resource: R, _ completionHandler: Response<V, E> -> Void) -> Response<V, E> -> Void {
+
+    return { response in
+        if let request = response.request, httpResponse = response.response, data = response.data {
+            let cachedResponse = CachedResponse(response: httpResponse,
+                                                data: data,
+                                                duration: 30,
+                                                storagePolicy: resource.cacheStoragePolicy)
+            cachedResponse.store(for: request, cache: resource.cache)
         }
+        completionHandler(response)
     }
 }
 
