faa873430e4f0876f97925b6185cb269869fae96
Fellipe Caetano <fellipe.caetano@movile.com>
2016-08-04 14:41:39 -0300

    Returning cached responses when possible

--- Wolf/Classes/Cache/Networking+Cache.swift
+++ Wolf/Classes/Cache/Networking+Cache.swift
@@ -4,19 +4,53 @@ public extension HTTPClient {
     func sendRequest<R: protocol<HTTPResource, CacheableResource>>
         (resource: R, completionHandler: Response<R.Value, R.Error> -> Void) -> Request {
 
-        return request(resource)
-            .validate()
-            .response(responseSerializer: resource.responseSerializer,
-                      completionHandler: cache(resource, completionHandler))
+        let requestToSend = request(resource)
+        if let urlRequest = requestToSend.request,
+            underlyingCachedResponse = resource.cache.cachedResponseForRequest(urlRequest) {
+
+            let cachedResponse = CachedResponse(cachedResponse: underlyingCachedResponse, duration: 30)
+            let result = resource.responseSerializer.serializeResponse(urlRequest,
+                                                                       cachedResponse.response as? NSHTTPURLResponse,
+                                                                       cachedResponse.data,
+                                                                       nil)
+            let response = Response(request: urlRequest,
+                                    response: cachedResponse.response as? NSHTTPURLResponse,
+                                    data: cachedResponse.data,
+                                    result: result)
+            completionHandler(response)
+            return requestToSend
+        } else {
+            return requestToSend
+                .validate()
+                .response(responseSerializer: resource.responseSerializer,
+                          completionHandler: cache(resource, completionHandler))
+        }
     }
 
     func sendArrayRequest<R: protocol<HTTPResource, CacheableResource>>
         (resource: R, completionHandler: Response<[R.Value], R.Error> -> Void) -> Request {
 
-        return request(resource)
-            .validate()
-            .response(responseSerializer: resource.arrayResponseSerializer,
-                      completionHandler: cache(resource, completionHandler))
+        let requestToSend = request(resource)
+        if let urlRequest = requestToSend.request,
+            underlyingCachedResponse = resource.cache.cachedResponseForRequest(urlRequest) {
+
+            let cachedResponse = CachedResponse(cachedResponse: underlyingCachedResponse, duration: 30)
+            let result = resource.arrayResponseSerializer.serializeResponse(urlRequest,
+                                                                            cachedResponse.response as? NSHTTPURLResponse,
+                                                                            cachedResponse.data,
+                                                                            nil)
+            let response = Response(request: urlRequest,
+                                    response: cachedResponse.response as? NSHTTPURLResponse,
+                                    data: cachedResponse.data,
+                                    result: result)
+            completionHandler(response)
+            return requestToSend
+        } else {
+            return request(resource)
+                .validate()
+                .response(responseSerializer: resource.arrayResponseSerializer,
+                          completionHandler: cache(resource, completionHandler))
+        }
     }
 }
 
@@ -36,6 +70,6 @@ private func cache<R: protocol<HTTPResource, CacheableResource>, V, E: ErrorType
 }
 
 public protocol CacheableResource {
-    var cache: NSURLCache { get }
+    var cache: URLCache { get }
     var cacheStoragePolicy: NSURLCacheStoragePolicy { get }
 }
