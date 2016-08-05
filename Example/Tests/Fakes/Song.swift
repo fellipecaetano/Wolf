660d56272645498e410c3af18df8ee76203f2779
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-08-05 14:52:07 -0300

    Unbox: object and array requests tested

--- Example/Tests/Fakes/Song.swift
+++ Example/Tests/Fakes/Song.swift
@@ -18,20 +18,45 @@ extension Song {
         typealias Error = NSError
 
         case getSong
+        case getSongs
 
         var path: String {
             switch self {
             case .getSong:
                 return "song"
+            case .getSongs:
+                return "songs"
             }
         }
 
         func serialize(data: NSData?, error: NSError?) -> Result<Song, NSError> {
-            return .Failure(NSError(domain: "", code: -1, userInfo: nil))
+            if let error = error {
+                return .Failure(error)
+            } else if let data = data {
+                do {
+                    let song: Song = try Unbox(data)
+                    return .Success(song)
+                } catch let error {
+                    return .Failure(error as NSError)
+                }
+            } else {
+                return .Failure(NSError(domain: "", code: -1, userInfo: nil))
+            }
         }
 
         func serializeArray(data: NSData?, error: NSError?) -> Result<[Song], NSError> {
-            return .Failure(NSError(domain: "", code: -1, userInfo: nil))
+            if let error = error {
+                return .Failure(error)
+            } else if let data = data {
+                do {
+                    let song: [Song] = try Unbox(data)
+                    return .Success(song)
+                } catch let error {
+                    return .Failure(error as NSError)
+                }
+            } else {
+                return .Failure(NSError(domain: "", code: -1, userInfo: nil))
+            }
         }
     }
 }
