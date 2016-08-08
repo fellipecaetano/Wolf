7af35d8b9e728d5e3d7b764e076b4033b8342e5b
Fellipe Caetano <fellipe.caetano@movile.com>
2016-08-08 20:59:17 -0300

    KeyedArchive and cache support

--- Wolf/Classes/Persistence/FileSystem.swift
+++ Wolf/Classes/Persistence/FileSystem.swift
@@ -1,10 +1,19 @@
 public protocol URLConvertible {
-    var baseURL: NSURL { get }
-    var path: String { get }
+    static var baseURL: NSURL { get }
+    static var path: String { get }
 }
 
 public extension URLConvertible {
-    var URL: NSURL {
+    static var URL: NSURL {
         return baseURL.URLByAppendingPathComponent(path)
+    }
+}
+
+public protocol Cacheable {}
+
+public extension Cacheable where Self: URLConvertible {
+    static var baseURL: NSURL {
+        return NSFileManager.defaultManager().URLsForDirectory(.CachesDirectory,
+                                                               inDomains: .UserDomainMask)[0]
     }
 }
