b6047abb36f63cffa13546b6a4c301f5b3a4401c
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-08-16 10:21:19 -0300

    Deleting the persistence layer from the library

--- Wolf/Classes/Persistence/Asynchronous.swift
+++
@@ -1,9 +0,0 @@
-public protocol Asynchronous {
-    var queue: dispatch_queue_t { get }
-}
-
-public extension Asynchronous {
-    func dispatch(block: Void -> Void) {
-        dispatch_async(queue, block)
-    }
-}
