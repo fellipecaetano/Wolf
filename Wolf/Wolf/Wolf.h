d26ceb3e6d02b3671247c9b5f6afd9844a453e95
Fellipe Caetano <fellipe.caetano4@gmail.com>
2016-11-16 21:54:26 -0200

    Create a new framework project

---
+++ Wolf/Wolf/Wolf.h
@@ -0,0 +1,19 @@
+//
+//  Wolf.h
+//  Wolf
+//
+//  Created by Felipe Caetano on 16/11/16.
+//  Copyright © 2016 Fellipe Caetano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wolf.
+FOUNDATION_EXPORT double WolfVersionNumber;
+
+//! Project version string for Wolf.
+FOUNDATION_EXPORT const unsigned char WolfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wolf/PublicHeader.h>
+
+
